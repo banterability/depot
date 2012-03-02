7aecd16c4fda4649597a49c3ecfda99d24ed5122
Jeffrey Long <jeff@banterability.com>
2012-03-02 09:47:52 -0600

    Add test coverage for JSON conversion

--- spec/compiled/localStorageSpec.js
+++ spec/compiled/localStorageSpec.js
@@ -1,21 +1,52 @@
 (function() {
-  var store;
-  store = new LocalStorageWrapper;
+  beforeEach(function() {
+    return this.store = new LocalStorageWrapper;
+  });
   describe("LocalStorageWrapper", function() {
-    describe("get()", function() {
-      return it("calls through to the getItem method", function() {
-        spyOn(localStorage, 'getItem');
-        spyOn(JSON, 'parse').andReturn("");
-        store.get('key');
-        return expect(localStorage.getItem).toHaveBeenCalledWith('key');
+    describe("wraps window.localStorage methods", function() {
+      describe("get()", function() {
+        return it("calls through to the getItem method", function() {
+          spyOn(localStorage, 'getItem');
+          spyOn(JSON, 'parse').andReturn("");
+          this.store.get('key');
+          return expect(localStorage.getItem).toHaveBeenCalledWith('key');
+        });
+      });
+      describe("set()", function() {
+        return it("calls through to the setItem method", function() {
+          spyOn(localStorage, 'setItem');
+          spyOn(JSON, 'stringify').andReturn("value");
+          this.store.set('key', 'value');
+          return expect(localStorage.setItem).toHaveBeenCalledWith('key', 'value');
+        });
+      });
+      return describe("delete()", function() {
+        return it("calls through to the removeItem method", function() {
+          spyOn(localStorage, 'removeItem');
+          this.store["delete"]('key');
+          return expect(localStorage.removeItem).toHaveBeenCalledWith('key');
+        });
       });
     });
-    return describe("set()", function() {
-      return it("calls through to the setItem method", function() {
+    return describe("converts complex types to & from JSON", function() {
+      beforeEach(function() {
+        this.nativeObj = {
+          album: "Mass Romantic",
+          band: "The New Pornographers",
+          year: 2000
+        };
+        return this.jsonObj = '{"album":"Mass Romantic","band":"The New Pornographers","year":2000}';
+      });
+      it("stores objects as JSON", function() {
         spyOn(localStorage, 'setItem');
-        spyOn(JSON, 'stringify').andReturn("value");
-        store.set('key', 'value');
-        return expect(localStorage.setItem).toHaveBeenCalledWith('key', 'value');
+        this.store.set('album', this.nativeObj);
+        return expect(localStorage.setItem).toHaveBeenCalledWith('album', this.jsonObj);
+      });
+      return it("gets JSON as objects", function() {
+        var response;
+        localStorage.setItem('album', this.jsonObj);
+        response = this.store.get('album');
+        return expect(response).toEqual(this.nativeObj);
       });
     });
   });
--- spec/localStorageSpec.coffee
+++ spec/localStorageSpec.coffee
@@ -1,17 +1,39 @@
-store = new LocalStorageWrapper
+beforeEach ->
+  @store = new LocalStorageWrapper
 
 describe "LocalStorageWrapper", ->
+  describe "wraps window.localStorage methods", ->
+    describe "get()", ->
+      it "calls through to the getItem method", ->
+        spyOn(localStorage, 'getItem')
+        spyOn(JSON, 'parse').andReturn("")
+        @store.get 'key'
+        expect(localStorage.getItem).toHaveBeenCalledWith('key')
 
-  describe "get()", ->
-    it "calls through to the getItem method", ->
-      spyOn(localStorage, 'getItem')
-      spyOn(JSON, 'parse').andReturn("")
-      store.get 'key'
-      expect(localStorage.getItem).toHaveBeenCalledWith('key')
+    describe "set()", ->
+      it "calls through to the setItem method", ->
+        spyOn(localStorage, 'setItem')
+        spyOn(JSON, 'stringify').andReturn("value")
+        @store.set 'key', 'value'
+        expect(localStorage.setItem).toHaveBeenCalledWith('key', 'value')
 
-  describe "set()", ->
-    it "calls through to the setItem method", ->
+    describe "delete()", ->
+      it "calls through to the removeItem method", ->
+        spyOn(localStorage, 'removeItem')
+        @store.delete 'key'
+        expect(localStorage.removeItem).toHaveBeenCalledWith('key')
+
+  describe "converts complex types to & from JSON", ->
+    beforeEach ->
+      @nativeObj = {album: "Mass Romantic", band: "The New Pornographers", year: 2000}
+      @jsonObj = '{"album":"Mass Romantic","band":"The New Pornographers","year":2000}'
+
+    it "stores objects as JSON", ->
       spyOn(localStorage, 'setItem')
-      spyOn(JSON, 'stringify').andReturn("value")
-      store.set 'key', 'value'
-      expect(localStorage.setItem).toHaveBeenCalledWith('key', 'value')
+      @store.set 'album', @nativeObj
+      expect(localStorage.setItem).toHaveBeenCalledWith('album', @jsonObj)
+
+    it "gets JSON as objects", ->
+      localStorage.setItem 'album', @jsonObj
+      response = @store.get 'album'
+      expect(response).toEqual(@nativeObj)
